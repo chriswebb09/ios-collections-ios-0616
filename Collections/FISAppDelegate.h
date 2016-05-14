3987aa1022dd2c79d3ff396a3fd4413631225fdb
chriswebb09 <chris.webb5249@gmail.com>
2016-05-14 05:49:13 -0400

    First commit.

--- Collections/FISAppDelegate.h
+++ Collections/FISAppDelegate.h
@@ -6,10 +6,28 @@
 
 @property (strong, nonatomic) UIWindow *window;
 
-/**
- 
- * Declare your methods here.
- 
- */
+- (NSArray *)arrayBySortingArrayAscending:(NSArray *)array;
+
+- (NSArray *)arrayBySortingArrayDescending:(NSArray *)array;
+
+- (NSArray *)arrayBySwappingFirstObjectWithLastObjectInArray:(NSArray *)array;
+
+- (NSArray *)arrayByReversingArray:(NSArray *)array;
+
+- (NSString *)stringInBasicLeetString:(NSString *)string;
+
+- (NSArray *)splitArrayIntoNegativesAndPositives:(NSArray *)array;
+
+- (NSArray *)namesOfHobbitsInDictionary:(NSDictionary *)hobbits;
+
+- (NSArray *)stringsBeginningWithAInArray:(NSArray *)array;
+
+- (NSUInteger)sumOfIntegersInArray:(NSArray *)array;
+
+- (NSArray *)arrayByPluralizingStringsInArray:(NSArray *)array;
+
+- (NSDictionary *)countsOfWordsInString:(NSArray *)array;
+
+- (NSDictionary *)songsGroupedByArtistFromArray:(NSArray *)array;
 
 @end
--- Collections/FISAppDelegate.m
+++ Collections/FISAppDelegate.m
@@ -10,10 +10,59 @@
     return YES;
 }
 
-/**
- 
- * Define your methods here.
- 
- */
+- (NSArray *)arrayBySortingArrayAscending:(NSArray *)array {
+    NSSortDescriptor *sortAscending = [NSSortDescriptor sortDescriptorWithKey:nil ascending:YES];
+    return [array sortedArrayUsingDescriptors:@[sortAscending]];
+}
+
+- (NSArray *)arrayBySortingArrayDescending:(NSArray *)array {
+    NSSortDescriptor *sortDescending = [NSSortDescriptor sortDescriptorWithKey:nil ascending:NO];
+    return [array sortedArrayUsingDescriptors:@[sortDescending]];
+}
+
+- (NSArray *)arrayBySwappingFirstObjectWithLastObjectInArray:(NSArray *)array {
+    NSMutableArray *mutableArray = [array mutableCopy];
+    NSUInteger index = [mutableArray count];
+    [mutableArray exchangeObjectAtIndex:0 withObjectAtIndex:index];
+    return mutableArray;
+}
+
+- (NSArray *)arrayByReversingArray:(NSArray *)array {
+    return [[array reverseObjectEnumerator] allObjects];
+}
+
+- (NSString *)stringInBasicLeetString:(NSString *)string {
+    return nil;
+}
+
+- (NSArray *)splitArrayIntoNegativesAndPositives:(NSArray *)array {
+    return nil;
+}
+
+- (NSArray *)namesOfHobbitsInDictionary:(NSDictionary *)hobbits {
+    return nil;
+}
+
+- (NSArray *)stringsBeginningWithAInArray:(NSArray *)array {
+    return nil;
+    
+}
+
+- (NSArray *)arrayByPluralizingStringsInArray:(NSArray *)array {
+    return nil;
+}
+
+- (NSDictionary *)countsOfWordsInString:(NSArray *)array {
+    NSMutableDictionary *counter = [[NSMutableDictionary alloc]init];
+    for (NSString *string in array) {
+        NSUInteger wordCount = [string count];
+        [counter setObject:string forKey:wordCount];
+    }
+    return nil;
+}
+
+- (NSDictionary *)songsGroupedByArtistFromArray:(NSArray *)array {
+    return nil;
+}
 
 @end
